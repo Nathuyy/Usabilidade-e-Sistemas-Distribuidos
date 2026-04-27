f8161296a3ab28a10d09e6265e70a4ac5bca2409
nathuuy <nathalia.cericatto@gmail.com>
2026-04-27 20:16:25 -0300

    ajustes

--- projetos/apps/orderitems/models.py
+++ projetos/apps/orderitems/models.py
@@ -3,16 +3,16 @@ from products.models import Product
 from orders.models import Order
 # Create your models here.
 class Orderitem(models.Model):
-    quantity = models.PositiveIntegerField('Quantidade',null=True, blank=True,default=0)
-    unitary_price = models.DecimalField('Preco unitario', max_digits=10, decimal_places=2)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    
+    quantity = models.IntegerField('Quantidade',null=True, blank=True,default=0)
+    unit_price = models.FloatField('Preco unitario',null=True, blank=True, default=0.0)
+    subtotal = models.FloatField('Preco unitario',null=True, blank=True, default=0.0)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
+
     class Meta:
-        unique_together = ('order', 'product')
         verbose_name = 'Item de pedido'
         verbose_name_plural = 'Itens de pedido'
         ordering =['id']
 
     def __str__(self):
-        return f'{self.quantity} - {self.product.name}'
+        return "%s" % (self.id) 
--- projetos/apps/orders/models.py
+++ projetos/apps/orders/models.py
@@ -5,16 +5,19 @@ from employees.models import Employee
 
 # Create your models here.
 class Order(models.Model):
-    payment_method = models.CharField('Forma de Pagamento', max_length=30, choices=[
-        ('boleto', 'Boleto'),
-        ('cartao', 'Cartão de Crédito'),
-        ('pix', 'PIX'),
+    created_at = models.DateTimeField(auto_now_add=True)
+    payment_method = models.CharField('Forma de pagamento', max_length=20, null=True, blank=True, default='Dinheiro',choices=[
+        ('Dinheiro', 'Dinheiro'),
+        ('Pix', 'Pix'),
+        ('Cartao de Credito', 'Cartao de Credito'),
+        ('Cartao de Debito', 'Cartao de Debito'),
     ])
-    status = models.CharField('Status', max_length=20, default='andamento', choices=[
-        ('andamento', 'Em andamento'),
-        ('finalizado', 'Finalizado'),
-        ('cancelado', 'Cancelado'),
+    status = models.CharField('Status', max_length=20, null=True, blank=True, default='Em andamento',choices=[
+        ('Em andamento', 'Em andamento'),
+        ('Finalizado', 'Finalizado'),
+        ('Cancelado', 'Cancelado'),
     ])
+    total = models.FloatField('Preco Total', null=True, blank=True, default=0.0)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     
@@ -24,4 +27,4 @@ class Order(models.Model):
         ordering =['id']
 
     def __str__(self):
-        return f'{self.client}' 
+        return "%s" % (self.id) 
